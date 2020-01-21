a3eb5e60f5c5a8592ccad990140f7924b109bda3
sunowsir <sunow.wnag@gmail.com>
2020-01-21 15:11:00 +0800

    update : setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,96 @@
 #!/bin/bash
 
-cd "${HOME}"
+SHELLOld="${SHELL}"
+WorkDir="${HOME}/.zsh"
+WorkDirOld="${HOME}/.zsh.old"
+ZshConf="${HOME}/.zshrc"
+ZshConfOld="${HOME}/.zshrc.old"
+AntigenFile="${WorkDir}/antigen.zsh"
 
-sudo apt-get install -y git curl zsh
+DetectionUrl="ping -c 3 -w 5 raw.githubusercontent.com"
+JumpToHome="cd ${HOME}"
+ChangeShellToZsh="chsh -s /bin/zsh"
+ChangeShellToOldShell="chsh -s ${SHELLOld}"
+InstallNeedPackage="sudo apt-get install -y git curl zsh"
+DownloadAntigen="curl -L git.io/antigen > ${AntigenFile}"
+DownloadZshConf="curl -L https://raw.githubusercontent.com/sunowsir/my-zsh-conf/master/.zshrc > ${ZshConf}"
 
-if [[ ! -d ~/.zsh ]]; 
-then
-	mkdir ~/.zsh/
-else 
-	mv -f ~/.zsh ~/.zsh.old
-fi
+function ExeVIPCmd() {
+	local cmd="${1}"
 
-curl -L git.io/antigen > ~/.zsh/antigen.zsh
+	if [[ $(eval "${cmd}") -ne 0 ]];
+	then
+		exit ${?}
+	fi
 
+	return 0
+}
 
-if [[ ! -f ~/.zshrc ]];
-then
-	mv -f ~/.zshrc ~/.zshrc_old
-fi
+function CreateWorkDir() {
+	if [[ -d "${WorkDir}" ]]; 
+	then
+		mv -f "${WorkDir}" "${WorkDirOld}"
+	fi
+	if [[ -f ${ZshConf} ]];
+	then
+		mv -f "${ZshConf}" "${ZshConfOld}"
+	fi
+	mkdir "${WorkDir}"
+}
 
-curl -L https://raw.githubusercontent.com/sunowsir/my-zsh-conf/master/.zshrc > ~/.zshrc
+function Install() {
+	ExeVIPCmd "${InstallNeedPackage}"
+	ExeVIPCmd "${ChangeShellToZsh}"
+	CreateWorkDir
+	ExeVIPCmd "${DownloadAntigen}"
+	ExeVIPCmd "${DownloadZshConf}"
+	return ${?}
+}
 
+function Uninstall() {
+	ExeVIPCmd "${ChangeShellToOldShell}"
+	if [[ -d "${WorkDirOld}" ]]; 
+	then
+		rm -rf "${WorkDir}"
+		mv -f "${WorkDirOld}" "${WorkDir}"
+	fi
+	if [[ -f "${ZshConfOld}" ]];
+	then
+		rm -rf "${ZshConf}"
+		mv -f "${ZshConfOld}" "${ZshConf}" 
+	fi
+}
+
+function Usage() {
+	echo "Usage : bash ./setup.sh [--install | --uninstall]"
+	echo "  --install : install the configuration."
+	echo "  --uninstall : Uninstall the configuration."
+}
+
+function Main() {
+	
+	if [[ $(eval "${DetectionUrl}") -ne 0 ]];
+	then
+		echo -e "\033[1;31mERROR\033[0m : Unable to connect to: raw.githubusercontent.com"
+		return ${?}
+	fi
+
+	eval "${JumpToHome}"
+
+	case ${1} in 
+		"--install")
+			Install
+		;;
+		"--uninstall")
+			Uninstall
+		;;
+		*)
+			Usage
+		;;
+	esac
+
+	return ${?}
+}
+
+Main "${@}"
+return ${?}
